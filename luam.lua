de978ee7acd73f7b885b8d2a8f284d1b97b17e0f
Git-Monke <javelasq2006@gmail.com>
2023-11-20 22:49:00 -0800

    Initial Commit

--- luam.lua
+++ luam.lua
@@ -1,30 +1,32 @@
 os.loadAPI("luam/Result.lua")
 
-local args = {...}
+local args = { ... }
 
+-- function recursive_print(input, depth)
+--     depth = depth or 0
+
+--     for key, entry in pairs(input) do
+--         if type(entry) == "table" then
+--             print(string.rep(" ", depth) .. key)
+--             recursive_print(entry, depth + 1)
+--         else
+--             print(string.rep(" ", depth) .. key)
+--         end
+--     end
+-- end
+
+-- Takes in a path and converts it to absolute given the current shell directory. If it's already absolute, leave it alone.
+-- Can only error if no path is provided
 function normalize_path(path)
     if not path then
         return Result.Err("No path provided")
     end
-   
+
     if not (string.sub(path, 1, 1) == "/") then
         path = fs.combine(shell.dir(), path)
     end
 
     return Result.Ok(path)
-end
-
-function recursive_print(input, depth)
-    depth = depth or 0
-    
-    for key, entry in pairs(input) do
-        if type(entry) == "table" then
-            print(string.rep(" ", depth) .. key)
-            recursivePrint(entry, depth + 1)
-        else
-            print(string.rep(" ", depth) .. key)
-        end
-    end
 end
 
 function build_package(dir)
@@ -35,22 +37,22 @@ function build_package(dir)
     if not fs.isDir(dir) then
         return Result.Err("Not a valid directory!")
     end
-    
+
     local found_toml = false
     local files = fs.list(dir)
     local output = {}
-    
+
     for _, file in pairs(files) do
         local path = dir .. "/" .. file
         local result
-        
+
         if file == "luam.toml" then
             found_toml = true
         end
-        
+
         if fs.isDir(path) then
             result = build_package(path):unwrap_or_error()
-            
+
             for subpath, contents in pairs(result) do
                 output[subpath] = contents
             end
@@ -60,7 +62,7 @@ function build_package(dir)
             reader.close()
         end
     end
-    
+
     if not found_toml then
         return Result.Err("No toml file found!")
     end
@@ -71,7 +73,7 @@ end
 function run_build(dir)
     local result = build_package(dir):unwrap_or_error()
     local output = textutils.serialise(result, nil, 4)
-    
+
     local writer = fs.open("output.txt", "w")
     writer.write(output)
     writer.close()
@@ -82,7 +84,7 @@ function init_project(name)
 
     local project_path = fs.combine(current_path, name)
     fs.makeDir(project_path)
-    
+
     local toml = fs.open(fs.combine(project_path, "luam.toml"), "w")
     toml.write('[package]\nname = "' .. name .. '"\nversion = "0.1.0"\n\n[dependencies]')
     toml.close()
@@ -90,6 +92,8 @@ function init_project(name)
     local src = fs.open(fs.combine(project_path, "src.lua"), "w")
     src.write('print("Hello World!")')
     src.close()
+
+    print("New project created at " .. project_path)
 end
 
 if not args[1] then
@@ -99,9 +103,14 @@ end
 if args[1] == "build" then
     local path = normalize_path(args[2]):unwrap_or_error()
     run_build(path)
+    return
 end
 
 if args[1] == "init" then
     local name = args[2] or error("No package name included!")
     init_project(name)
+    return
 end
+
+error("Invalid command")
+
